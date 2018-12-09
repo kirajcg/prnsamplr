bcb739134bc9ad5ddb09b7e5de74cea4f10d36a9
Kira Coder Gylling <kira.gylling@gmail.com>
2018-12-09 18:29:28 +0100

    this time SRS in C++ works for sure

--- src/prnsamplr.cpp
+++ src/prnsamplr.cpp
@@ -2,7 +2,6 @@
 
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
-//using namespace arma;
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
@@ -19,19 +18,28 @@ Rcpp::DataFrame srs_cpp(Rcpp::DataFrame df,
   
   
   int ntot = id.size();
-  arma::uvec strat = arma::find_unique(stratid);
+  arma::uvec strat = arma::find_unique(arma::sort(stratid));
   int nstrat = strat.size();
   Rcpp::LogicalVector sampled(ntot, 1);
   Rcpp::LogicalVector sampled_sorted(ntot, 1);
+  Rcpp::IntegerVector strat_tot(nstrat+1, 1);
   
-  arma::vec stratid_sorted = stratid(arma::stable_sort_index(prn));
-  arma::vec id_sorted = id(arma::stable_sort_index(prn));
-  arma::vec nsamp_sorted = nsamp(arma::stable_sort_index(prn));
+  arma::vec id_sorted = id(arma::sort_index(prn));
+  arma::vec nsamp_sorted = nsamp(arma::sort_index(prn));
+  arma::vec stratid_sorted = stratid(arma::sort_index(prn));
+  arma::vec id_sorted_st = id_sorted(arma::stable_sort_index(stratid_sorted));
+  arma::vec nsamp_sorted_st = nsamp_sorted(arma::stable_sort_index(stratid_sorted));
+  
+  strat_tot[0] = 0;
+  for (int i = 1; i <= nstrat; i++) {
+    strat_tot[i] = strat_tot[i-1] + arma::sum(stratid == stratid[strat[i-1]]);
+  }
   
   for (int i = 0; i < nstrat; i++) {
-    for (int j = 0; j < ntot; j++) {
-      if (stratid[j] == stratid[i]) {
-        if (j <= nsamp_sorted[j]) {
+    for (int j = strat_tot[i]; j < strat_tot[i+1]; j++) {
+      if (stratid[j] == stratid[strat[i]]) {
+        int ind_strat = j - strat_tot[i];
+        if (ind_strat < nsamp_sorted_st[j]) {
           sampled_sorted[j] = TRUE;
         }
         else {
@@ -41,14 +49,17 @@ Rcpp::DataFrame srs_cpp(Rcpp::DataFrame df,
     }
   }
   
+  
   for (int i = 0; i < ntot; i++) {
     for (int j = 0; j < ntot; j++) {
-      if (id[i] == id_sorted[j]) {
+      if (id[i] == id_sorted_st[j]) {
         sampled[i] = sampled_sorted[j];
       }
     }
   }
-
+  
+  Rcpp::DataFrame out;
+  out["id"] = id_sorted_st;
   df["sampled"] = sampled;
   return df;
 }
