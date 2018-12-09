9d886d305b57cdcec92e5cc5c98416499ba948d2
Kira Coder Gylling <kira.gylling@gmail.com>
2018-12-09 16:00:51 +0100

    C++ SRS implementation works!

--- src/prnsamplr.cpp
+++ src/prnsamplr.cpp
@@ -2,33 +2,53 @@
 
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
+//using namespace arma;
 
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
-
-// Does nothing useful so far
+// First draft
 // [[Rcpp::export]]
 Rcpp::DataFrame srs_cpp(Rcpp::DataFrame df,
-                        std::string stratid,
-                        std::string nsamp,
-                        std::string prn){
-  Rcpp::CharacterVector strata = df[stratid] ;
-  Rcpp::NumericVector   prns   = df[prn] ;
-  Rcpp::IntegerVector   n      = df[nsamp] ;
+                        arma::vec stratid,
+                        arma::vec nsamp,
+                        arma::vec prn,
+                        arma::vec id){
   
-  std::unordered_set<SEXP> tab(strata.begin(), strata.end());
-  int nstrat = tab.size();
   
-  int ntot = prns.size();
-  Rcpp::LogicalVector sampled(ntot,1);
+  int ntot = id.size();
+  arma::uvec strat = arma::find_unique(stratid);
+  int nstrat = strat.size();
+  Rcpp::LogicalVector sampled(ntot, 1);
+  Rcpp::LogicalVector sampled_sorted(ntot, 1);
   
-  for (int i=0; i<ntot; i++) {
-    sampled(i) = TRUE;
+  arma::vec stratid_sorted = stratid(arma::stable_sort_index(prn));
+  arma::vec id_sorted = id(arma::stable_sort_index(prn));
+  arma::vec nsamp_sorted = nsamp(arma::stable_sort_index(prn));
+  
+  for (int i = 0; i < nstrat; i++) {
+    for (int j = 0; j < ntot; j++) {
+      if (stratid[j] == stratid[i]) {
+        if (j <= nsamp_sorted[j]) {
+          sampled_sorted[j] = TRUE;
+        }
+        else {
+          sampled_sorted[j] = FALSE;
+        }
+      }
+    }
   }
   
+  for (int i = 0; i < ntot; i++) {
+    for (int j = 0; j < ntot; j++) {
+      if (id[i] == id_sorted[j]) {
+        sampled[i] = sampled_sorted[j];
+      }
+    }
+  }
+
   df["sampled"] = sampled;
   return df;
 }
