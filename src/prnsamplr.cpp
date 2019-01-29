38493ce113df1b855500d68ed15031107aef923b
kirajcg <kira.gylling@gmail.com>
2019-01-29 20:39:38 +0100

    refactored pps to something else that doesn't work

--- src/prnsamplr.cpp
+++ src/prnsamplr.cpp
@@ -22,22 +22,75 @@ Rcpp::DataFrame pps_cpp(Rcpp::DataFrame df,
                         arma::vec prn,
                         arma::vec size,
                         arma::vec id){
-  bool iterated = FALSE;
   int ntot = id.size();
+  arma::vec stratid_num;
+  arma::vec strat;
+  arma::vec lambda;
+  arma::vec sum_size;
+  arma::vec Q;
+  int nstrat;
   
-  arma::vec stratid_num = (arma::vec) stratid;
-  arma::vec strat = arma::unique(arma::sort(stratid_num));
-  int nstrat = strat.size();
+  Rcpp::LogicalVector sampled_gtone;
+  arma::vec stratid_gtone;
+  arma::vec nsamp_gtone;
+  arma::vec prn_gtone;
+  arma::vec size_gtone;
+  arma::vec id_gtone;
   
-  arma::vec lambda(ntot, 1);
+  arma::vec stratid_old;
+  arma::vec nsamp_old;
+  arma::vec prn_old;
+  arma::vec size_old;
+  arma::vec id_old;
   
-  arma::vec sum_size = arma::zeros(ntot);
+  arma::vec lambda_gtone;
+  arma::vec Q_gtone;
+  int n_gtone;
   
-  arma::vec Q(ntot, 1);
+  arma::vec strat_ltone;
+  arma::vec stratid_ltone;
+  arma::vec nsamp_ltone;
+  arma::vec prn_ltone;
+  arma::vec size_ltone;
+  arma::vec id_ltone;
+  arma::vec lambda_ltone;
+  arma::vec Q_ltone;
+  arma::vec sum_size_ltone;
+  arma::vec strat_tot_ltone;
+  int nstrat_ltone;
+  int n_ltone;
+  
+  
+  arma::vec strat_tot;
+  arma::vec id_sorted;
+  arma::vec nsamp_sorted;
+  arma::vec stratid_sorted;
+  arma::vec id_sorted_st;
+  arma::vec nsamp_sorted_st;
+  
+  arma::vec id_int;
+  arma::vec Q_int;
+  arma::vec Q_out;
+  arma::vec lambda_int;
+  arma::vec lambda_out;
+  Rcpp::LogicalVector sampled_int(ntot, 1);
+  Rcpp::LogicalVector sampled_out(ntot, 1);
+  
+  arma::uvec find_ltone;
+  arma::uvec find_gtone;
+  
+  
+  stratid_num = (arma::vec) stratid;
+  strat = arma::unique(arma::sort(stratid_num));
+  lambda = arma::zeros(ntot);
+  sum_size = arma::zeros(ntot);
+  Q = arma::zeros(ntot);
+  nstrat = strat.size();
+  
   
   for (int i=0; i<ntot; i++) {
     for (int j=0; j<ntot; j++) {
-      for (int k=0; k<ntot; k++) {
+      for (int k=0; k<nstrat; k++) {
         if (stratid_num[j] == strat[k]) {
           sum_size[i] += size[j];
         }
@@ -49,28 +102,57 @@ Rcpp::DataFrame pps_cpp(Rcpp::DataFrame df,
     lambda[i] = nsamp[i] * size[i] / sum_size[i];
   }
   
-  if (arma::any(lambda >= 1)) {
-    iterated = TRUE;
+  
+  do {
+    find_gtone = arma::find(lambda >= 1);
+    find_ltone = arma::find(lambda < 1);
     
-    arma::vec stratid_gtone = stratid(arma::find(lambda >= 1));
-    arma::vec nsamp_gtone = nsamp(arma::find(lambda >= 1));
-    arma::vec prn_gtone = prn(arma::find(lambda >= 1));
-    arma::vec size_gtone = size(arma::find(lambda >= 1));
-    arma::vec id_gtone = id(arma::find(lambda >= 1));
-    arma::vec lambda_gtone = arma::ones(id_gtone.size());
-    arma::vec Q_gtone = arma::zeros(id_gtone.size());
+    if (find_gtone.size() > 0) { 
+      stratid_old = stratid_gtone;
+      nsamp_old = nsamp_gtone;
+      prn_old = prn_gtone;
+      size_old = size_gtone;
+      id_old = id_gtone;
+      
+      stratid_gtone = arma::join_cols(stratid_old, stratid.elem(find_gtone));
+      nsamp_gtone = arma::join_cols(nsamp_old, nsamp.elem(find_gtone));
+      prn_gtone = arma::join_cols(prn_old, prn.elem(find_gtone));
+      size_gtone = arma::join_cols(size_old, size.elem(find_gtone));
+      id_gtone = arma::join_cols(id_old, id.elem(find_gtone));
+      
+      n_gtone = id_gtone.size();
+      lambda_gtone = arma::ones(n_gtone);
+      Q_gtone = arma::zeros(n_gtone);
+      
+      sampled_gtone(n_gtone, 1);
+      for (int i=0; i<n_gtone; i++) {
+        sampled_gtone[i] = TRUE;
+      }
+    }
     
-    arma::vec sampled_gtone_n = arma::ones(id_gtone.size());
-    arma::ivec sampled_gtone = arma::conv_to<arma::ivec>::from(sampled_gtone_n);
     
-    arma::vec stratid_ltone = stratid(arma::find(lambda < 1));
-    arma::vec nsamp_ltone = nsamp(arma::find(lambda < 1));
-    arma::vec prn_ltone = prn(arma::find(lambda < 1));
-    arma::vec size_ltone = size(arma::find(lambda < 1));
-    arma::vec id_ltone = id(arma::find(lambda < 1));
+    stratid_ltone = stratid.elem(find_ltone);
+    nsamp_ltone = nsamp.elem(find_ltone);
+    prn_ltone = prn.elem(find_ltone);
+    size_ltone = size.elem(find_ltone);
+    id_ltone = id.elem(find_ltone);
     
-    int n_ltone = id_ltone.size();
-    int n_gtone = id_gtone.size();
+    n_ltone = id_ltone.size();
+    stratid_num = (arma::vec) stratid_ltone;
+    strat = arma::unique(arma::sort(stratid_num));
+    lambda = arma::zeros(n_ltone);
+    sum_size_ltone = arma::zeros(n_ltone);
+    nstrat_ltone = strat.size();
+    
+    for (int i=0; i<n_ltone; i++) {
+      for (int j=0; j<n_ltone; j++) {
+        for (int k=0; k<nstrat_ltone; k++) {
+          if (stratid_num[j] == strat[k]) {
+            sum_size_ltone[i] += size_ltone[j];
+          }
+        }
+      }
+    }
     
     for (int i=0; i<n_ltone; i++) {
       for (int j=0; j<n_gtone; j++) {
@@ -80,105 +162,87 @@ Rcpp::DataFrame pps_cpp(Rcpp::DataFrame df,
       }
     }
     
-    Rcpp::DataFrame df_ltone = Rcpp::DataFrame::create(Rcpp::Named("stratid")=stratid_ltone, 
-                                                       Rcpp::Named("nsamp")=nsamp_ltone,
-                                                       Rcpp::Named("prn")=prn_ltone,
-                                                       Rcpp::Named("size")=size_ltone,
-                                                       Rcpp::Named("id")=id_ltone);
-    
-    Rcpp::DataFrame df_ltone_out = pps_cpp(df=df_ltone,
-                                           stratid=stratid_ltone,
-                                           nsamp=nsamp_ltone,
-                                           prn_ltone,
-                                           size_ltone,
-                                           id_ltone);
-    
-    arma::vec Q_ltone = df_ltone_out["Q"];
-    arma::vec lambda_ltone = df_ltone_out["lambda"];
-    arma::ivec sampled_ltone = df_ltone_out["sampled"];
-    
-    arma::vec stratid_out = arma::join_cols(stratid_gtone, stratid_ltone);
-    arma::vec nsamp_out = arma::join_cols(nsamp_gtone, nsamp_ltone);
-    arma::vec prn_out = arma::join_cols(prn_gtone, prn_ltone);
-    arma::vec size_out = arma::join_cols(size_gtone, size_ltone);
-    arma::vec id_out = arma::join_cols(id_gtone, id_ltone);
-    arma::vec Q_out = arma::join_cols(Q_gtone, Q_ltone);
-    arma::vec lambda_out = arma::join_cols(lambda_gtone, lambda_ltone);
-    arma::ivec sampled_out_i = arma::join_cols(sampled_gtone, sampled_ltone);
-    
-    Rcpp::LogicalVector sampled_out;
-    for (int i = 0; i < ntot; i++) {
-      sampled_out[i] = sampled_out_i[i] == 1;
+    for (int i=0; i<n_ltone; i++) {
+      lambda[i] = nsamp_ltone[i] * size_ltone[i] / sum_size_ltone[i];
     }
-    
-    Rcpp::DataFrame df_out = Rcpp::DataFrame::create(Rcpp::Named("stratid")=stratid_out,
-                                                     Rcpp::Named("nsamp")=nsamp_out,
-                                                     Rcpp::Named("prn")=prn_out,
-                                                     Rcpp::Named("size")=size_out,
-                                                     Rcpp::Named("id")=id_out,
-                                                     Rcpp::Named("Q")=Q_out,
-                                                     Rcpp::Named("lambda")=lambda_out,
-                                                     Rcpp::Named("sampled")=sampled_out);
-    
-    return df_out;
+  } while (arma::any(lambda >= 1));
+  
+  
+  Q_ltone = arma::zeros(n_ltone);
+  for (int i=0; i<n_ltone; i++) {
+    Q_ltone[i] = prn_ltone[i] * (1 - lambda[i]) / (lambda[i] * (1 - prn_ltone[i]));
   }
   
-  else {
-    for (int i=0; i<ntot; i++) {
-      Q[i] = prn[i] * (1 - lambda[i]) / (lambda[i] * (1 - prn[i]));
-    }
-    
-    arma::ivec sampled_int(ntot, 1);
-    Rcpp::LogicalVector sampled_bool(ntot, 1);
-    
-    arma::ivec sampled_sorted(ntot, 1);
-    arma::vec strat_tot(nstrat+1, 1);
-    
-    arma::vec id_sorted = id(arma::sort_index(Q));
-    arma::vec nsamp_sorted = nsamp(arma::sort_index(Q));
-    arma::vec stratid_sorted = stratid(arma::sort_index(Q));
-    arma::vec id_sorted_st = id_sorted(arma::stable_sort_index(stratid_sorted));
-    arma::vec nsamp_sorted_st = nsamp_sorted(arma::stable_sort_index(stratid_sorted));
-    
-    strat_tot[0] = 0;
-    for (int i = 1; i <= nstrat; i++) {
-      strat_tot[i] = strat_tot[i-1] + arma::sum(stratid == stratid[strat[i-1]]);
-    }
-    
-    for (int i = 0; i < nstrat; i++) {
-      for (int j = strat_tot[i]; j < strat_tot[i+1]; j++) {
-        if (stratid[j] == stratid[strat[i]]) {
-          int ind_strat = j - strat_tot[i];
-          if (ind_strat < nsamp_sorted_st[j]) {
-            sampled_sorted[j] = 1;
-          }
-          else {
-            sampled_sorted[j] = 0;
-          }
-        }
-      }
-    }
-    
-    
-    for (int i = 0; i < ntot; i++) {
-      for (int j = 0; j < ntot; j++) {
-        if (id[i] == id_sorted_st[j]) {
-          sampled_int[i] = sampled_sorted[j]; 
-          sampled_bool[i] = sampled_sorted[j] == 1;
-        }
-      }
-    }
-    
-    df["Q"] = Q;
-    df["lambda"] = lambda;
-    if (iterated) {
-      df["sampled"] = sampled_int;
-    }
-    else {
-      df["sampled"] = sampled_bool;
-    }
-    return df;
+  strat_ltone = arma::unique(arma::sort(stratid_ltone));
+  nstrat_ltone = strat_ltone.size();
+  Rcpp::LogicalVector sampled_sorted(n_ltone, 1);
+  Rcpp::LogicalVector sampled_ltone(n_ltone, 1);
+  strat_tot = arma::zeros(nstrat_ltone+1);
+  
+  id_sorted = id_ltone(arma::sort_index(Q_ltone));
+  nsamp_sorted = nsamp_ltone(arma::sort_index(Q_ltone));
+  stratid_sorted = stratid_ltone(arma::sort_index(Q_ltone));
+  
+  id_sorted_st = id_sorted(arma::stable_sort_index(stratid_sorted));
+  nsamp_sorted_st = nsamp_sorted(arma::stable_sort_index(stratid_sorted));
+  
+  strat_tot[0] = 0;
+  for (int i = 1; i <= nstrat_ltone; i++) {
+    strat_tot[i] = strat_tot[i-1] + arma::sum(stratid_ltone == stratid_ltone[strat_ltone[i-1]]);
   }
+  
+  
+  for (int i = 0; i < nstrat_ltone; i++) {
+    for (int j = strat_tot[i]; j < strat_tot[i+1]; j++) {
+      if (stratid_ltone[j] == stratid_ltone[strat_ltone[i]]) {
+        int ind_strat_ltone = j - strat_tot[i];
+        if (ind_strat_ltone < nsamp_sorted_st[j]) {
+          sampled_sorted[j] = TRUE;
+        }
+        else {
+          sampled_sorted[j] = FALSE;
+        }
+      }
+    }
+  }
+  
+  for (int i = 0; i < n_ltone; i++) {
+    for (int j = 0; j < n_ltone; j++) {
+      if (id_ltone[i] == id_sorted_st[j]) {
+        sampled_ltone[i] = sampled_sorted[j];
+      }
+    }
+  }
+  
+  id_int = arma::join_cols(id_gtone, id_ltone);
+  Q_int = arma::join_cols(Q_gtone, Q_ltone);
+  lambda_int = arma::join_cols(lambda_gtone, lambda);
+  Q_out = arma::zeros(ntot);
+  lambda_out = arma::zeros(ntot);
+  
+  for (int i = 0; i < n_gtone; i++) {
+    sampled_int[i] = sampled_gtone[i];
+  }
+  
+  for (int i = n_gtone; i < n_ltone+n_gtone; i++) {
+    sampled_int[i] = sampled_ltone[i];
+  }
+  
+  for (int i = 0; i < ntot; i++) {
+    for (int j = 0; j <ntot; j++) {
+      if (id_int[i] == id[j]) {
+        Q_out[j] = Q_int[i];
+        lambda_out[j] = lambda_int[i];
+        sampled_out[j] = sampled_int[i];
+      }
+    }
+  }
+  
+  df["lambda"] = lambda_out;
+  df["Q"] = Q_out;
+  df["sampled"] = sampled_out;
+  
+  return df;
 }
 
 
