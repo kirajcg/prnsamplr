453fe5a7517a48033705c41509a8181d8389eca1
kirajcg <kira.gylling@gmail.com>
2019-02-16 14:41:01 +0100

    I don't want to bother with C++ anymore

--- R/pps.R
+++ R/pps.R
@@ -13,15 +13,18 @@ pps <- function(df, stratid, nsamp, prn, size) {
     gtone <- subset(df, df$lambda >= 1)
     gtone$Q <- rep(NA, nrow(gtone))
     gtone$sampled <- rep(TRUE, nrow(gtone))
+    gtone["nnew"] <- NULL
     ltone <- pps(subset(df, df$lambda < 1), stratid, "nnew", prn, size)
+    ltone["nnew"] <- NULL
     dfout <- rbind(gtone, ltone)
-    dfout["nnew"] <- NULL
+    #dfout["nnew"] <- NULL
     return(dfout)
   }
 
   else {
     df["Q"] <- df[prn] * (1 - df["lambda"]) / (df["lambda"] * (1 - df[prn]))
     df <- df[with(df, order(df[stratid], df["Q"])), ]
+    #df <- df[with(df, order(stratid, Q)), ]
     df$sampled <- sequence(rle(as.character(df[, stratid]))$lengths) <= df[nsamp]
     df["sumsize"] <- NULL
     return(df)
